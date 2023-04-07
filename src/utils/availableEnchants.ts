07739f5375113c260624c492895dbf677fb7218f
ytiK0 <tiprson@gmail.com>
2023-04-07 11:21:34 +0300

    relogick prepared

---
+++ src/utils/availableEnchants.ts
@@ -0,0 +1,5 @@
+export type availableEnchants = "e" | "w"
+
+export type SwordAvailableEnchants = "Bane of Arthropods" | "Looting" | "Fire Aspect"
+	| "Smite" | "Sharpness" | "Knockback"
+	| "Mending" | "Unbreaking"
